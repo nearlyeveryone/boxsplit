5f79e95e3fd054f956161ba0f893eb37ca8b9c94
MBacon15 <39793714+MBacon15@users.noreply.github.com>
2019-11-05 17:01:47 -0500

    Add DB models
    An attempt at adding in the Split and Box class as pydantic models for validation. I still need to set the required params as required and such

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,14 +1,26 @@
 from enum import Enum
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, condecimal
+from typing import List
+import datetime
 
 
-class Shape(str, Enum):
-    square = "square"
-    circle = "circle"
-    triangle = "triangle"
-    rectangle = "triangle"
-    dodecahedron = "dodecahedron"
+class Split(BaseModel):
+    split_id: int
+    name: str
+    description: str
+    price_cents: condecimal(decimal_places=2)
+    creation_date: datetime
+
+
+class Box(BaseModel):
+    box_id: int
+    name: str
+    description: str
+    state: int = 0
+    creation_date: datetime
+
+    splits: List[Split]
 
 
 app = FastAPI()
@@ -19,22 +31,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
 
-@app.get("/shape/{shape_name}")
-async def get_shape(shape_name: Shape):
-    """
-    - **shape_name**: What shape are you using?
-    """
-    if shape_name == Shape.circle:
-        return {"shape_name": shape_name, "message": "I have infinite vertices!"}
-    if shape_name == Shape.square:
-        return {"shape_name": shape_name, "message": "I have 4 vertices."}
-    if shape_name == Shape.rectangle:
-        return {"shape_name": shape_name, "message": "I have 4 vertices and L E N G T H!"}
-    if shape_name == Shape.triangle:
-        return {"shape_name": shape_name, "message": "I have 3 vertices."}
-    return {"shape_name": shape_name, "message": "Who am I, why am I here? :("}
+

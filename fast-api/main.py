a6d9e811081f68378d3bf6ab16db02190a825699
MBacon15 <39793714+MBacon15@users.noreply.github.com>
2019-11-22 16:05:27 -0500

    More work

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,16 +1,16 @@
 from enum import Enum
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel, condecimal
 from typing import List
-import datetime
+from datetime import datetime
 
 
 class Split(BaseModel):
     split_id: int
-    name: str
-    description: str
-    price_cents: condecimal(decimal_places=2)
-    creation_date: datetime
+    name: str = 'TestSplit'
+    description: str = 'I have no description'
+    price_cents: condecimal(decimal_places=2) = 19.99
+    creation_date: datetime = datetime.now()
 
 
 class Box(BaseModel):
@@ -26,11 +26,16 @@ class Box(BaseModel):
 app = FastAPI()
 
 
+@app.get("/splits")
+def create_split(split: Split):
+    return split
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"This is": "the root"}
 
 
-
-
-
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}

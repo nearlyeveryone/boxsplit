77ba97edfe76a19dc9cff53a2d5603a5e54501a9
kooz2@rpi.edu <kooz2@rpi.edu>
2019-12-10 22:49:56 -0500

    adding boxsplit db locally

--- python-api/main.py
+++ python-api/main.py
@@ -11,9 +11,7 @@ def main():
     secrets = read_secrets()
     mongo_setup.global_init(secrets)
 
-    print(svc.create_user("poog", "poog@split.box").name)
-
-    # print(svc.find_user_by_email('poog@split.box'))
+    print(svc.find_user_by_email('poog@split.box'))
     # user = svc.find_user_by_email("poog@split.box")
     # box = svc.find_box_by_id("5dbc8670e3e74473175d283f")
     # split = svc.find_split_by_id("5dbc8670e3e74473175d283e")

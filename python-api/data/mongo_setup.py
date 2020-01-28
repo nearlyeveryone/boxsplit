6324383ce10f12c24f9c63dee0b4eb668d271934
kooz2@rpi.edu <kooz2@rpi.edu>
2020-01-28 16:24:15 -0500

    adding boxsplit db locally

--- python-api/data/mongo_setup.py
+++ python-api/data/mongo_setup.py
@@ -3,4 +3,4 @@ import mongoengine
 
 def global_init(secrets):
     mongoengine.connect(db=secrets["db"], username=secrets["username"], password=secrets["password"], host=secrets["host"], name="boxsplit")
-    # mongoengine.connect(db=secrets["db"], name='boxsplit')
+    # mongoengine.connect(db=secrets["db"], name='boxsplit') 
